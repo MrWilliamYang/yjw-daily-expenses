f3d62ae6cb7100aad677f07def90914ccf03fe05
MrWilliamYang <1832900365@qq.com>
2018-10-30 11:21:41 +0800

    新增花销，用户测试类

--- src/test/java/com/yjw/interview/service/ExpensesServiceTest.java
+++ src/test/java/com/yjw/interview/service/ExpensesServiceTest.java
@@ -1,6 +1,14 @@
 package com.yjw.interview.service;
 
+import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.yjw.interview.common.ResponseErrorEnum;
+import com.yjw.interview.pojo.vo.BaseResponse;
+import com.yjw.interview.pojo.vo.DeleteExpensesReqVO;
+import com.yjw.interview.pojo.vo.ExpensesReqVO;
+import com.yjw.interview.pojo.vo.FindAllExpensesListByReqVO;
+import com.yjw.interview.pojo.vo.FindAmountByPersonIdReqVO;
 
 /**
  * 花销测试类
@@ -14,7 +22,108 @@ public class ExpensesServiceTest {
 
     @Autowired
     IExpensesService expensesService;
-    
-    
 
+    /**
+     * 添加花销
+     */
+    public void insertExpenses() {
+        ExpensesReqVO request = new ExpensesReqVO();
+
+        // 添加成功
+        request.setAmount("4");
+        request.setName("4");
+        request.setPersonId(1);
+        request.setPrice("4");
+        request.setQuantity("4");
+        BaseResponse result = expensesService.insertExpenses(request);
+        System.out.println(result);
+        Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
+
+        // 添加失败
+        request.setAmount("4");
+        request.setName("4");
+        request.setPersonId(0);
+        request.setPrice("4");
+        request.setQuantity("4");
+        BaseResponse result1 = expensesService.insertExpenses(request);
+        System.out.println(result1);
+        Assert.assertEquals(ResponseErrorEnum.DATE_ADD_WRONG, result.getCode());
+    }
+
+    /**
+     * 查询花销列表
+     */
+    public void findAllExpensesList() {
+
+        // 查询成功
+        BaseResponse result = expensesService.findAllExpensesList();
+        System.out.println(result);
+        Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
+    }
+
+    /**
+     * 根据id查询花销列表
+     */
+    public void findAllExpensesListBy() {
+        FindAllExpensesListByReqVO request = new FindAllExpensesListByReqVO();
+
+        // 查询成功
+        request.setPersonId(6);
+        BaseResponse result = expensesService.findAllExpensesListBy(request);
+        System.out.println(result);
+        Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
+
+        // 查询失败
+        request.setPersonId(0);
+        BaseResponse result1 = expensesService.findAllExpensesListBy(request);
+        System.out.println(result1);
+        Assert.assertEquals(ResponseErrorEnum.DATA_NOT_EXIST.getErrorCode(), result1.getCode());
+    }
+
+    /**
+     * 根据花销id删除花销
+     */
+    public void deleteExpenses() {
+        DeleteExpensesReqVO request = new DeleteExpensesReqVO();
+
+        // 删除成功
+        request.setId(10);
+        BaseResponse result = expensesService.deleteExpenses(request);
+        System.out.println(result);
+        Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
+
+        // 删除失败
+        request.setId(10);
+        BaseResponse result1 = expensesService.deleteExpenses(request);
+        System.out.println(result1);
+        Assert.assertEquals(ResponseErrorEnum.DATE_DELETE_WRONG.getErrorCode(), result1.getCode());
+    }
+
+    /**
+     * 通过 personid查询总价
+     */
+    public void findAmountByPersonId() {
+        FindAmountByPersonIdReqVO request = new FindAmountByPersonIdReqVO();
+
+        // 查询成功
+        request.setPersonId(1);
+        BaseResponse result = expensesService.findAmountByPersonId(request);
+        System.out.println(result);
+        Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
+
+        // 查询成功
+        request.setPersonId(0);
+        BaseResponse result1 = expensesService.findAmountByPersonId(request);
+        System.out.println(result1);
+        Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
+    }
+
+    /**
+     * 删除所有花销
+     */
+    public void deleteAllExpenses() {
+        BaseResponse result = expensesService.deleteAllExpenses();
+        System.out.println(result);
+        Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
+    }
 }
--- src/test/java/com/yjw/interview/service/PersonServiceTest.java
+++ src/test/java/com/yjw/interview/service/PersonServiceTest.java
@@ -7,20 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.yjw.interview.YjwInterviewQuestionsApplicationTests;
 import com.yjw.interview.common.ResponseErrorEnum;
 import com.yjw.interview.pojo.vo.BaseResponse;
+import com.yjw.interview.pojo.vo.DeletePersonReqVO;
 import com.yjw.interview.pojo.vo.FindPersonInfoReqVO;
 import com.yjw.interview.pojo.vo.PersonRegisterReqVO;
+import com.yjw.interview.pojo.vo.UpdatePersonInfoReqVO;
 
 /**
  * 人物服务测试类
  * 
+ * @description PersonServiceTest.java
  * @author YangJianWei
- * @version $Id: PersonServiceTest.java, v 0.1 2018年10月9日 下午4:25:32 YangJianWei Exp $
+ * @date 2018年10月9日 下午4:25:32
+ * @version $Id: v 0.1
  */
 public class PersonServiceTest extends YjwInterviewQuestionsApplicationTests {
 
     @Autowired
     IPersonService personService;
 
+    /**
+     * 用户注册测试
+     */
     @Test
     public void savePerson() {
         PersonRegisterReqVO request = new PersonRegisterReqVO();
@@ -34,24 +41,91 @@ public class PersonServiceTest extends YjwInterviewQuestionsApplicationTests {
         System.out.println(result);
         Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
     }
-    
+
+    /**
+     * 查询用户详情
+     */
     @Test
     public void findPersonInfo() {
         FindPersonInfoReqVO request = new FindPersonInfoReqVO();
-        
+
         //查询成功
         request.setId(1);
         BaseResponse result = personService.findPersonInfo(request);
         System.out.println(result);
         Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
-        
+
         //查询失败
         request.setId(0);
         BaseResponse result1 = personService.findPersonInfo(request);
         System.out.println(result);
         Assert.assertEquals(ResponseErrorEnum.DATA_NOT_EXIST.getErrorCode(), result1.getCode());
     }
-    
-    
 
+    /**
+     * 修改用户信息
+     */
+    public void updatePersonInfo() {
+        UpdatePersonInfoReqVO request = new UpdatePersonInfoReqVO();
+
+        // 修改成功
+        request.setAge(7);
+        request.setAvatar("7");
+        request.setGender(2);
+        request.setId(6);
+        request.setName("7");
+        request.setPassword("7");
+        request.setTel("7");
+        BaseResponse result = personService.updatePersonInfo(request);
+        System.out.println(result);
+        Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
+
+        // 修改失败
+        request.setAge(7);
+        request.setAvatar("7");
+        request.setGender(2);
+        request.setId(0);
+        request.setName("7");
+        request.setPassword("7");
+        request.setTel("7");
+        BaseResponse result1 = personService.updatePersonInfo(request);
+        System.out.println(result1);
+        Assert.assertEquals(ResponseErrorEnum.DATE_UPDATE_WRONG.getErrorCode(), result1.getCode());
+
+    }
+
+    /**
+     * 删除用户信息
+     */
+    public void deletePersonInfo() {
+        DeletePersonReqVO request = new DeletePersonReqVO();
+
+        // 删除成功
+        request.setId(6);
+        BaseResponse result = personService.deletePerson(request);
+        System.out.println(result);
+        Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
+
+        // 删除失败
+        request.setId(0);
+        BaseResponse result1 = personService.deletePerson(request);
+        System.out.println(result1);
+        Assert.assertEquals(ResponseErrorEnum.DATE_DELETE_WRONG.getErrorCode(), result1.getCode());
+    }
+
+    /**
+     * 查询用户列表
+     */
+    public void findAllPersonList() {
+
+        // 查询成功
+        BaseResponse result = personService.findAllPersonList();
+        System.out.println(result);
+        Assert.assertEquals(BaseResponse.SUCCESS_CODE, result.getCode());
+
+//        // 查询失败
+//        BaseResponse result1 = personService.findAllPersonList();
+//        System.out.println(result1);
+//        Assert.assertEquals(ResponseErrorEnum.DATA_NOT_EXIST.getErrorCode(), result1.getCode());
+    }
 }
